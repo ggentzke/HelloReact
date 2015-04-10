68b5f9a281daed74fac1bd71c40775a0505952d4
Glenn Gentzke <glenn.gentzke@gmail.com>
2015-04-10 12:20:11 -0400

    React Native Hellow World project

---
+++ index.ios.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var HelloReact = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+Control+Z for dev menu
+        </Text>
+        <Text style={styles.hello}>
+          This is Glenns Hello World!
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  hello: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: 'red'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  },
+});
+
+AppRegistry.registerComponent('HelloReact', () => HelloReact);
